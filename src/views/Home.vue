21ede416f54f7302fec21516a82f3a96b1427b22
IVCA <ivca80web@outlook.com>
2025-09-03 12:03:20 +0200

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div><h2>Home Page</h2></div>
-  <a class="veza" href="/public/Inicijalni.docx">Inicijalni test 6. razred</a> <br>
+  <a class="veza" href="/src/dokumenti/Inicijalni.docx">Inicijalni test 6. razred</a> <br>
   <a class="veza" href="/public/V-razred.pdf">V-razred</a>
 
 </template>
